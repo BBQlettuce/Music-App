229757bf541d20e6a289aa219358e4e91b929fbe
App Academy Student <appacademy@Apps-Mac-mini-16.local>
2015-08-20 10:35:39 -0400

    create User model

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true, message: "password too short"}
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    token = SecureRandom.urlsafe_base64
+    until User.find_by(session_token: token).nil?
+      token = SecureRandom.urlsafe_base64
+    end
+    token
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+end
