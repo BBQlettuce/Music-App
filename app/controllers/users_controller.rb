2eb217ad23710aadebdf867ec6d30e2af8220f4d
Oscar Liu <oscar.liu.13q@gmail.com>
2015-08-20 10:59:50 -0400

    write new and create actions for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params)
+    @user = User.new(user_params)
+    if @user.save
+      # log the user in
+      render :show
+    else
+      # flash.now[:errors] = @user.errors.full_messages
+      redirect_to new_user_url
+    end
   end
 
   def show
+    render :show
   end
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def render_auth_token
+    html = <<-HTML.html_safe
+      <input
+        type="hidden"
+        name="authenticity_token"
+        value ="#{form_authenticity_token}">
+    HTML
+    html
+  end
+
 end
